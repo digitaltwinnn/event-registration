4cca348b985baf1ab75dd2a5b8443fd50a6f1c34
digitaltwinnn <45341252+digitaltwinnn@users.noreply.github.com>
2019-02-12 15:34:28 +0100

    sample data from node.js server

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var path = require ('path');
+var events = require('./eventsController');
 var app = express();
 var rootpath = path.normalize(__dirname + '/../');
+var bodyParser = require('body-parser');
 
 app.use(express.static(rootpath + '/app'));
+
+app.get('/data/event/:id', events.get);
+app.post('/data/event/:id', events.save);
 
 app.listen(8080);
 console.log('Listening on port 8080...');
