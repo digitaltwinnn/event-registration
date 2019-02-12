07455d2d30cf474482c57ccc09bfbb0db78f213a
digitaltwinnn <45341252+digitaltwinnn@users.noreply.github.com>
2019-02-12 14:49:06 +0100

    added a gravatar url service
    Completed untill the section 'Using Angular's $http Service' in chapter 'Creating and Using Angular Services'

---
+++ app/js/services/EventData.js
@@ -0,0 +1,43 @@
+eventsApp.factory('eventData', function() {
+
+    return {
+        event: {
+            name: 'Angular Boot Camp',
+            date: 1359781015626,
+            time: '10:30 am',
+            price: 34,
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Directive Masterclass',
+                    creatorName: "Both Smith",
+                    duration: 1,
+                    level: "Advanced",
+                    abstract: "Learn the ins and outs of Directives. More Text More Text More Text More Text More Text More Text",
+                    upvoteCount: 0
+                },
+                {
+                    name: 'Scope for fun and profit',
+                    creatorName: "John Doe",
+                    duration: 2,
+                    level: "Introductory",
+                    abstract: "Learn about scope within Angular JS. More Text More Text More Text More Text",
+                    upvoteCount: 0
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: "Jane Doe",
+                    duration: 4,
+                    level: "Intermediate",
+                    abstract: "Understand how Controllers behave well. More Text More Text More More Text More Text More Text More Text More Text More Text More Text More Text",
+                    upvoteCount: 0
+                }
+            ]
+        }
+    };
+});
